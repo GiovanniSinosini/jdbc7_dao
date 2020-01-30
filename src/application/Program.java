7891c477c30d947a2aa92ad55d9bac7651e0108c
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-30 21:03:54 +0000

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,8 +14,9 @@ public class Program {
 		Department obj = new Department(1, "Books");
 		
 		Seller seller = new Seller(1, "Gio", "gio@gmail.com", new Date(), 3000.0, obj);
+
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
-		
 	}
 }
