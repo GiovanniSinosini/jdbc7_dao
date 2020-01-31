386761423be665e398a2bbdd660ced98419f6547
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-31 18:35:19 +0000

    find by department implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -95,11 +98,51 @@ public class SellerDaoJDBC implements SellerDao {
 	}
 
 
-
 	@Override
 	public List<Seller> findAll() {
 		
 		return null;
 	}
 
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName\r\n" + 
+					"FROM seller INNER JOIN department\r\n" + 
+					"ON seller.DepartmentId = department.Id\r\n" + 
+					"WHERE DepartmentId = ?\r\n" + 
+					"ORDER BY Name"
+					);
+			
+			st.setInt(1, department.getId());
+			rs = st.executeQuery();
+			List <Seller> list = new ArrayList<>();
+			Map<Integer, Department> map = new HashMap<>();  //empty map
+			
+			while (rs.next()) {
+				
+				Department dep = map.get(rs.getInt("DepartmentId"));  // checks if it has already been
+				
+				if (dep == null) {        // control not to instantiate the departmentId more than once
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("DepartmentId"), dep); // save instantiated on the map
+				}
+				Seller obj = instantiateSeller(rs, dep);
+				list.add(obj);
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
 }
