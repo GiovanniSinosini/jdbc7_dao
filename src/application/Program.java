92eb19d9cea66d6eef7c3eda297f8c207b5bd084
Giovanni Sinosini <carvalho.sino@gmail.com>
2020-01-31 19:57:41 +0000

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,12 @@ public class Program {
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		seller = sellerDao.findById(1);    // search id
+		seller.setName("Martha Waine");  // new name
+		sellerDao.update(seller);        // insert into the database
+		System.out.println("Update completed");
+		
 	}
 }
